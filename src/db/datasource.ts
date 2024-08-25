6ce2723fe913c0a175ddd8444e3e162db4d8b891
Paramveer12 <paramveer.2023@gmail.com>
2024-08-26 02:35:49 +0530

    db migration

--- src/db/datasource.ts
+++ src/db/datasource.ts
@@ -3,14 +3,14 @@ import { Contact}  from "../entity/contact";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: "dpg-cr5p98dumphs73e70ovg-a",
   port: 5432,
-  username: "postgres" ,
-  password: "postgres"   ,
-  database: "bitespeed_db",
+  username: "user" ,
+  password: "JC7pxdnJRf6UG5RyIr7tcJTYLCsqoAbZ"   ,
+  database: "bitespeed_db_bwdj",
   synchronize: true,  // Automatically synchronize the schema
   logging: false,
   entities: [Contact],
-  migrations: [],
+  migrations: ["src/db/migrations/*{.ts,.js}"],
   subscribers: [],
 });
