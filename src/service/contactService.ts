e181993b1d0526cb05a6227dbacf565f10e8bccd
Paramveer12 <paramveer.2023@gmail.com>
2024-08-26 03:01:11 +0530

    minor fixes

--- src/service/contactService.ts
+++ src/service/contactService.ts
@@ -28,28 +28,35 @@ export class ContactService {
         }
 
         const existingContact = existingContacts.find(contact => contact.email === email && contact.phoneNumber === phoneNumber);
-        const primaryContact = existingContacts.find(contact => contact.linkPrecedence === "primary") || existingContacts[0];
+        var primaryContact = existingContacts.find(contact => contact.linkPrecedence === "primary");
+        if (primaryContact === null && primaryContact === undefined) {
+            var dbPrimaryContact = await this.contactRepository.findOne({
+                where:
+                    { id: existingContacts[0].linkedId}
+            });
+            primaryContact = dbPrimaryContact!
+        }
         var secondaryContacts = await this.contactRepository.find({
             where:
-                { linkedId: primaryContact.id }
+                { linkedId: primaryContact!.id }
         });
 
         if (existingContact != null && existingContact != undefined) {
-            return this.formatResponse(primaryContact, secondaryContacts);
+            return this.formatResponse(primaryContact!, secondaryContacts);
         }
 
         const existingContactWithEmail = existingContacts.find(contact => contact.email === email);
         const existingContactWithPhone = existingContacts.find(contact => contact.phoneNumber === phoneNumber);
 
-        if ((existingContactWithEmail != null && existingContactWithEmail != undefined) && (existingContactWithPhone != null && existingContactWithPhone != undefined))  {
+        if ((existingContactWithEmail != null && existingContactWithEmail != undefined) && (existingContactWithPhone != null && existingContactWithPhone != undefined)) {
             var secondaryPrimary = ""
-            if (existingContactWithEmail.linkPrecedence == "primary" && existingContactWithEmail.id != primaryContact.id ){
+            if (existingContactWithEmail.linkPrecedence == "primary" && existingContactWithEmail.id != primaryContact!.id) {
                 secondaryPrimary = existingContactWithEmail.id!
-            } else if (existingContactWithEmail.linkPrecedence == "secondary" && existingContactWithEmail.linkedId != primaryContact.id ){
+            } else if (existingContactWithEmail.linkPrecedence == "secondary" && existingContactWithEmail.linkedId != primaryContact!.id) {
                 secondaryPrimary = existingContactWithEmail.linkedId!
-            } else if (existingContactWithPhone.linkPrecedence == "primary" && existingContactWithPhone.id != primaryContact.id ){
+            } else if (existingContactWithPhone.linkPrecedence == "primary" && existingContactWithPhone.id != primaryContact!.id) {
                 secondaryPrimary = existingContactWithPhone.id!
-            } else if (existingContactWithPhone.linkPrecedence == "secondary" && existingContactWithPhone.linkedId != primaryContact.id ){
+            } else if (existingContactWithPhone.linkPrecedence == "secondary" && existingContactWithPhone.linkedId != primaryContact!.id) {
                 secondaryPrimary = existingContactWithPhone.linkedId!
             }
 
@@ -58,34 +65,34 @@ export class ContactService {
                     { id: secondaryPrimary }
             });
             var truePrimary = primaryContact
-            if (secondaryPrimaryContact!.createdAt > primaryContact.createdAt) {
+            if (secondaryPrimaryContact!.createdAt > primaryContact!.createdAt) {
                 secondaryPrimaryContact!.linkPrecedence = "secondary"
-                secondaryPrimaryContact!.linkedId = primaryContact.id
+                secondaryPrimaryContact!.linkedId = primaryContact!.id
                 await this.contactRepository.save(secondaryPrimaryContact!);
-                await this.contactRepository.update({ linkedId: secondaryPrimaryContact!.id }, { linkedId: primaryContact.id });
+                await this.contactRepository.update({ linkedId: secondaryPrimaryContact!.id }, { linkedId: primaryContact!.id });
             } else {
-                primaryContact.linkPrecedence = "secondary"
+                primaryContact!.linkPrecedence = "secondary"
                 primaryContact!.linkedId = secondaryPrimaryContact!.id
                 await this.contactRepository.save(primaryContact!);
-                await this.contactRepository.update({ linkedId: primaryContact.id }, { linkedId: secondaryPrimaryContact!.id });
+                await this.contactRepository.update({ linkedId: primaryContact!.id }, { linkedId: secondaryPrimaryContact!.id });
                 truePrimary = secondaryPrimaryContact!
             }
             secondaryContacts = await this.contactRepository.find({
                 where:
-                    { linkedId: truePrimary.id }
+                    { linkedId: truePrimary!.id }
             });
-            return this.formatResponse(truePrimary, secondaryContacts);
+            return this.formatResponse(truePrimary!, secondaryContacts);
         }
 
         const newContact = this.contactRepository.create({
             email,
             phoneNumber,
             linkPrecedence: "secondary",
-            linkedId: primaryContact.id
+            linkedId: primaryContact!.id
         });
         await this.contactRepository.save(newContact);
         secondaryContacts.push(newContact)
-        return this.formatResponse(primaryContact, secondaryContacts);
+        return this.formatResponse(primaryContact!, secondaryContacts);
     }
 
     private formatResponse(primaryContact: Contact, secondaryContacts: Contact[]) {
